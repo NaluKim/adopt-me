aaeb7177c17cca7c92bb2997f3b972cc38336602
NaluKim <nalu.kekoa1@gmail.com>
2023-09-01 17:19:50 -1000

    Mapping Through Dta with Hooks

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
-import react from '@vitejs/plugin-react';
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-    plugin: [react()],
-    root: 'src',
-})
+  plugins: [react()],
+  root: "src",
+});
